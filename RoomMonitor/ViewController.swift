cfe3fa20e5390b31289890826e172ac90252429f
Bruce Colby <bruce.colby.bc@gmail.com>
2019-07-26 18:44:28 -0400

    Initial Commit

---
+++ RoomMonitor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RoomMonitor
+//
+//  Created by Bruce Colby on 7/26/19.
+//  Copyright © 2019 Bruce Colby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
