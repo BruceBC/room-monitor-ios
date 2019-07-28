eebe5f51bc89daa22142b5ad6d08f03a7502062f
Bruce Colby <bruce.colby.bc@gmail.com>
2019-07-27 23:28:00 -0400

    — Updated ViewController to interact with core

--- RoomMonitor/ViewController.swift
+++ RoomMonitor/ViewController.swift
@@ -7,14 +7,146 @@
 //
 
 import UIKit
+import AnyCodable
+import RoomMonitorCore
 
 class ViewController: UIViewController {
-
+    // MARK: - IBOutlets
+    @IBOutlet weak var hardwareConnectionIndicatorView: UIView!
+    @IBOutlet weak var websocketConnectedIndicatorView: UIView!
+    @IBOutlet weak var occupiedIndicatorView: UIView!
+    
+    // MARK: - Private Properties
+    private var interactor: WebsocketInteractor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
+        setupInteractor()
     }
-
-
 }
 
+// MARK: - Setup
+extension ViewController {
+    func setupViews() {
+        hardwareConnectionIndicatorView.layer.cornerRadius = 10
+        websocketConnectedIndicatorView.layer.cornerRadius = 10
+        occupiedIndicatorView.layer.cornerRadius           = 10
+    }
+    
+    func setupInteractor() {
+        interactor = WebsocketInteractor(url: "ws://localhost:4040/app", delegate: self)
+    }
+}
+
+// MARK: - UI
+extension ViewController {
+    func displayMonitor(monitor: Monitor?) {
+        guard let monitor = monitor else { return }
+        occupiedIndicatorView.backgroundColor = monitor.present ? .green : .red
+    }
+}
+
+// MARK: WebsocketInteractorDelegate
+extension ViewController: WebsocketInteractorDelegate {
+    func connected() {
+        print("Connected")
+        websocketConnectedIndicatorView.backgroundColor = .green
+    }
+    
+    func disconncted() {
+        print("Disconnected")
+        hardwareConnectionIndicatorView.backgroundColor = .red
+        websocketConnectedIndicatorView.backgroundColor = .red
+        occupiedIndicatorView.backgroundColor           = .red
+    }
+    
+    func received(result: Result<WebsocketMessageResponse, Error>) {
+        switch result {
+        case .success(let response):
+            print(response)
+            route(response: response)
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+    
+    func received(result: Result<WebsocketDataResponse, Error>) {
+        switch result {
+        case .success(let response):
+            print(response)
+            route(response: response)
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
+
+// MARK: Websocket Router
+extension ViewController {
+    func route(response: WebsocketMessageResponse) {
+        switch response.type {
+        case "ready":
+            pair()
+        case "paired":
+            setMax()
+        case "max":
+            getMonitor()
+        case "connected":
+            hardwareConnected()
+        case "disconnected":
+            hardwareDisconnected()
+        case "process_not_started":
+            processNotStarted()
+        case "error":
+            error(message: response.message)
+        default:
+            print("Unkown type")
+        }
+    }
+    
+    func route(response: WebsocketDataResponse) {
+        switch response.type {
+        case "monitor":
+            displayMonitor(monitor: Monitor.decode(response: response))
+            getMonitor()
+        default:
+            print("Unkown type")
+        }
+    }
+    
+    func pair() {
+        interactor.pair(hardwareId: "50e90b91c72a8a6531900e6c0b842ef3")
+    }
+    
+    func setMax() {
+        interactor.setMax(max: 80)
+    }
+    
+    func getMonitor() {
+        // Short delay before fetching again
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.interactor.getMonitor()
+        }
+    }
+    
+    func hardwareConnected() {
+        hardwareConnectionIndicatorView.backgroundColor = .green
+    }
+    
+    func hardwareDisconnected() {
+        hardwareConnectionIndicatorView.backgroundColor = .red
+    }
+    
+    func processNotStarted() {
+        // Hardware not yet connected, need to try to update max again.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.setMax()
+        }
+    }
+    
+    func error(message: String) {
+        print("error: \(message)")
+    }
+}
